a0fb990ab512764dbecf0e2ad93d40f120dbd8e2
dgs2135 <166383268+dgs2135@users.noreply.github.com>
2026-05-23 21:14:43 +0100

    Update settings.py

--- riftwood_project/settings.py
+++ riftwood_project/settings.py
@@ -123,7 +123,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_URL = '/users/login/'
 
